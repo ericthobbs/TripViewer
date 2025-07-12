d14c8a72f15e45464a79556a5f0085d590504527
Eric Hobbs <eric@badpointer.net>
2025-07-12 04:48:57 -0700

    Tweaked the Help Window used to display command line help.

--- TripView/HelpWindow.xaml.cs
+++ TripView/HelpWindow.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace TripView
 {
@@ -22,9 +10,14 @@ namespace TripView
         public string HelpTextFromCommandLine { get; set; }
         public HelpWindow(string helptext)
         {
-            HelpTextFromCommandLine = helptext;
+            HelpTextFromCommandLine = helptext; //TODO: this should be a View Model.
             DataContext = this;
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
