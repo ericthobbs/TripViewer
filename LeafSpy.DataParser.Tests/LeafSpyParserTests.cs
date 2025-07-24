eecd4dc81ab84fded00d6d7a56361366c7d7f643
Eric Hobbs <eric@badpointer.net>
2025-07-24 10:02:34 -0700

    Add Verify snapshot testing tool.

--- LeafSpy.DataParser.Tests/LeafSpyParserTests.cs
+++ LeafSpy.DataParser.Tests/LeafSpyParserTests.cs
@@ -3,27 +3,28 @@
 namespace LeafSpy.DataParser.Tests
 {
     [TestClass]
-    public sealed class LeafSpyParserTests
+    [UsesVerify]
+    public sealed partial class LeafSpyParserTests
     {
         private readonly LeafspyImportConfiguration _config = new() { DistanceUnit = DistanceUnit.FEET, CsvDelimiter = "," };
 
+        [TestMethod]
+        public Task Run() => VerifyChecks.Run();
+
         [DataTestMethod]
-        [DataRow(@"..\\..\\..\\..\\examples\Log.csv", 964, 1, "1932", "68", 7612)]
-        public void LeafSpyParser_SingleTrip_ExampleLog_Valid(string filename, int expectedRecordCount, int recordNumber, string expectedRawElevation, string expectedRawSpeed, int expectedRpm)
+        [DataRow(@"..\\..\\..\\..\\examples\Log.csv")]
+        public async Task LeafSpyParser_SingleTrip_ExampleLog_Valid(string filename)
         {
             var fullPath = System.IO.Path.GetFullPath(filename);
             var parser = new LeafSpySingleTripParser(_config);
             parser.Open(fullPath);
 
-            var records = parser.Read().ToList();
-            var theRecord = records.Skip(recordNumber - 1).First();
+            var records = parser.Read();
 
-            Assert.AreEqual(expectedRecordCount, records.Count);
-            Assert.IsNotNull(theRecord);
+            var verifySettings = new VerifySettings();
+            verifySettings.UseTextForParameters(System.IO.Path.GetFileNameWithoutExtension(filename));
 
-            Assert.AreEqual(expectedRawElevation, theRecord.GpsPhoneElevation.RawValue);
-            Assert.AreEqual(expectedRawSpeed, theRecord.GpsPhoneSpeed.RawValue);
-            Assert.AreEqual(expectedRpm, theRecord.RPM);
+            await Verify(records, verifySettings);
         }
     }
 }
